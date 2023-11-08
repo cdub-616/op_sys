f31edd335be7c99c78e7069744339ab451c14d8a
cdub-616 <cdub616@aol.com>
2023-11-08 08:31:06 -0800

    Update RoundRobin.java

--- CPU_scheduling/src/scheduler_pack/RoundRobin.java
+++ CPU_scheduling/src/scheduler_pack/RoundRobin.java
@@ -14,7 +14,8 @@ public class RoundRobin {
        int numberOfProcesses, String[] processArray) {
 
         //convert process data strings to 2D array
-        final int P_DATA = 4;  
+        final int P_DATA = 4, procNumIndex = 0, procArTimeIndex = 1, 
+            burstIndex = 2;  
         int[][] processes = new int[numberOfProcesses][P_DATA];
         for (int i = 0; i < numberOfProcesses; i++) {
             String[] processStrTokens = processArray[i].split(" ");
@@ -23,56 +24,68 @@ public class RoundRobin {
             }
         }
         Queue<Integer> fifo = new LinkedList<>();  
+        LinkedList<Integer> procNotStarted = new LinkedList<>();
+        /*for (int i = 0; i < numberOfProcesses; i++) {
+            procNotStarted.add(processes[i][procNumIndex]);
+        }*/
         output.add(firstLine);
         
         //sort processes for queue
-        /*int timerSort = 0, counter = 0;
+        int timerSort = 0, counter = 0;
         while (counter < numberOfProcesses) {  
             for (int i = 0; i < numberOfProcesses; i++) {
-                int arrivalTime = processes[i][1];
+                int arrivalTime = processes[i][procArTimeIndex];
                 if (arrivalTime == timerSort) {
-                    fifo.add(processes[i][0]);
+                    procNotStarted.add(processes[i][procNumIndex]);
                     counter++;                     
                 }
             }
             timerSort++;
-        }*/
+        }
+        for (int k: procNotStarted)
+            System.out.println("list" + k);
         //simulate scheduling and CPU
         int time = 0;
-        boolean[] hasStarted = new boolean[numberOfProcesses];
+        //boolean[] hasStarted = new boolean[numberOfProcesses];
         
         //add processes to queue at arrival time 0
-        for (int i = 0; i < numberOfProcesses; i++) {
-            int arrivalTime = processes[i][1];
+        /*for (int i = 0; i < numberOfProcesses; i++) {
+            int arrivalTime = processes[i][procArTimeIndex];
             if (arrivalTime == time) {
-                fifo.add(processes[i][0]);
+                fifo.add(processes[i][procNumIndex]);
                 hasStarted[i] = true;
             }
-        }
+        }*/
         for (int k: fifo)
             System.out.println("queue: " + k);
         int[] waitingTimeArr = new int[numberOfProcesses];
         boolean[] isFinished = new boolean[numberOfProcesses];
-        while (!fifo.isEmpty()) {
+        while (!fifo.isEmpty() && !procNotStarted.isEmpty()) {
         
-            //add processes to queue at later arrival times
+            //add processes to queue time
             for (int i = 0; i < numberOfProcesses; i++) {
-                int arrivalTime = processes[i][1];
-                if (arrivalTime <= time && hasStarted[i] == false) {
-                    fifo.add(processes[i][0]);
-                    hasStarted[i] = true;
+                int arrivalTime = processes[i][procArTimeIndex];
+                if (arrivalTime <= time) {
+                    int procNum = processes[i][procNumIndex];
+                    fifo.add(procNum);
+                    for (int j = 0; j < procNotStarted.size(); j++) {
+                        if (procNotStarted.get(j) == procNum) {
+                            procNotStarted.remove(j);
+                        }
+                    }
+                    //hasStarted[i] = true;
                 }
             }
             for (int k: fifo)
                 System.out.println("queue: " + k);
             int processNumber = fifo.poll(), processIndex = processNumber - 1;
-            int cpuBurst = processes[processIndex][2];
+            int cpuBurst = processes[processIndex][burstIndex];
             String timeStr = Integer.toString(time);
             String pNumStr = Integer.toString(processNumber);
             String cpuLine = timeStr + " " + pNumStr;
             output.add(cpuLine);
-            if (processes[processIndex][2] > quantumLength) {  //not finished
-                processes[processIndex][2] = cpuBurst - quantumLength;
+            if (processes[processIndex][burstIndex] > quantumLength) {  //not finished
+                processes[processIndex][burstIndex] = cpuBurst - quantumLength;
                 fifo.add(processNumber);
                 time += quantumLength;
             }
